1333b3b8789c9b6cbd43c171365e75ec1e96e512
thiago-miguel <thiago.alexandre@outlook.com.br>
2025-12-22 13:18:08 -0300

    Estrutura do JS, com GET e POST via crudcrud

---
+++ cadastro.js
@@ -0,0 +1,50 @@
+const listaDeClientes = document.getElementById('lista-clientes');
+const formCadastro = document.getElementById('form-cadastro');
+const inputNome = document.getElementById('nome');
+const inputEmail = document.getElementById('email');
+
+formCadastro.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nome = inputNome.value;
+    const email = inputEmail.value;
+
+    const novoCliente = {
+        nome: nome,
+        email: email
+    };
+
+    //POST
+    fetch('https://crudcrud.com/api/d0c9f1e6f26b414597a96ceaf50573ea/cadastro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoCliente)
+    })
+    .then((response) => response.json())
+    .then((clienteCadastrado) => {
+        adicionarClienteNaLista(clienteCadastrado);
+        formCadastro.reset();
+    })
+
+    .catch(error => console.error('Erro ao cadastrar cliente:', error));
+
+});
+
+//GET
+fetch('https://crudcrud.com/api/d0c9f1e6f26b414597a96ceaf50573ea/cadastro')
+.then(response => response.json())
+.then((listaClientes) => {
+    console.log(listaClientes);
+    listaClientes.forEach((cliente) => {
+        adicionarClienteNaLista(cliente);
+    })
+    .catch(error => console.error('Erro ao buscar clientes:', error));
+})
+
+function adicionarClienteNaLista(cliente) {
+    const li = document.createElement('li');
+    li.innerHTML = `Nome: ${cliente.nome} | Email: ${cliente.email} <button>Excluir</button>`;
+    listaDeClientes.appendChild(li);
+}
